d4fdb8325e79efd2ca2470bde7fb4258fa7e1986
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-04-18 19:04:34 +0200

    modificado persona.java

--- src/Test05/Persona.java
+++ src/Test05/Persona.java
@@ -8,10 +8,16 @@ public class Persona {
 	private int edad;
 	private char sexo;
 	private boolean presentado;
+	private int probabilidadAbsencia;
 
 	// Constructor
 
-	public Persona() {
+	public Persona(int edad) {
+		this.nombre = generarNombreApellido();
+		this.edad = edad;
+		this.sexo = generarSexo();
+		this.probabilidadAbsencia = 0;
+		this.presentado = presentarse();
 
 	}
 
@@ -53,17 +59,46 @@ public class Persona {
 	}
 
 	// Methods
-	
+
 	/**
 	 * 
 	 * @param probabilidad - probabilidad de no presentarse
 	 */
-	private void presentarse(int probabilidad) {
+	private boolean presentarse() {
 		double num = Math.random() * 100;
-		
+
 		// Se presenta si el numero aleatorio del 0 al 100
-		// es mayor que la probabilidad de no presentarse
-		setPresentado(num > probabilidad);
+		// es mayor que la probabilidad de no presentarse a clase
+		if (num > this.probabilidadAbsencia)
+			return true;
+		return false;
+	}
+
+	/**
+	 * Generar un nombre para Espectador
+	 * 
+	 * @return
+	 */
+	private static String generarNombreApellido() {
+		String[] nombres = { "Pepe", "Juan", "Maria", "Isabel", "Carlos", "David", "Cristina" };
+		String[] apellido = { "Sanchez", "Perez", "Gallego", "Marin", "Martinez", "Collado", "Marquez" };
+
+		String nombreCompleto = nombres[Helpers.random(0, 6)] + " " + apellido[Helpers.random(0, 6)];
+
+		return nombreCompleto;
+	}
+
+	/**
+	 * Generar genero aleatorio
+	 * 
+	 * @return
+	 */
+	private char generarSexo() {
+		if (Math.random() >= 0.5) {
+			return 'H';
+		} else {
+			return 'M';
+		}
 	}
 
 }
