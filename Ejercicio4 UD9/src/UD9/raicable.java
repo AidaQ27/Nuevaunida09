cd20d5edeb70e4b3126c2e275d7ec486b1f1baee
GerardSanchezV <gerardsanchezvelasco@gmail.com>
2022-04-19 13:32:47 +0200

    UD9

--- Ejercicio4 UD9/src/UD9/raicable.java
+++ Ejercicio4 UD9/src/UD9/raicable.java
@@ -1,5 +1,18 @@
 package UD9;
 
-public class raicable {
-
+public interface raicable {
+	
+	public double getDiscriminante() ;
+	
+	public void obtenerRaices();
+	
+	public void obtenerRaiz();
+	
+	public boolean tieneRaices();
+	
+	public boolean tieneRaiz();
+	
+	public void calcular();
+	
 }
+
