520a84fdb9830bf401ab392470c5d9cc9859c04d
GerardSanchezV <gerardsanchezvelasco@gmail.com>
2022-04-19 13:29:04 +0200

    UD9

--- Ejercicio4 UD9/src/UD9/MainApp.java
+++ Ejercicio4 UD9/src/UD9/MainApp.java
@@ -8,20 +8,20 @@ public class MainApp {
 		Raices raiz1=new Raices(5,-1,3);
 		System.out.println("Raiz 1");
 		raiz1.calcular();
-		raiz1.ObtenerRaices();
-		raiz1.ObtenerRaiz();
+		raiz1.obtenerRaices();
+		raiz1.obtenerRaiz();
 		//segunda raiz
 		Raices raiz2=new Raices(-4,7,2);
 		System.out.println("\nRaiz 2");
 		raiz2.calcular();
-		raiz2.ObtenerRaices();
-		raiz2.ObtenerRaiz();
+		raiz2.obtenerRaices();
+		raiz2.obtenerRaiz();
 		//tercera raiz
 		Raices raiz3=new Raices(4,0,-4);
 		System.out.println("\nRaiz 3");
 		raiz3.calcular();
-		raiz3.ObtenerRaices();
-		raiz3.ObtenerRaiz();
+		raiz3.obtenerRaices();
+		raiz3.obtenerRaiz();
 
 	}
 
